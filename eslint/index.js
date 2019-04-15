f68b6373006ecd72c10f360fcb9231ca99bf6d32
Anatoli Huseu <avg.tolik@gmail.com>
2019-04-15 16:14:46 +0300

    add config setup

---
+++ eslint/index.js
@@ -0,0 +1,35 @@
+// eslint config
+
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  plugins: ["jest", "react-hooks"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    "jest/globals": true
+  },
+  rules: {
+    "prefer-const": "warn",
+    "react/prop-types": [
+      "warn",
+      {
+        ignore: ["dispatch"]
+      }
+    ],
+    "no-unused-vars": ["error", { ignoreRestSiblings: true }],
+    "react/jsx-no-target-blank": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  globals: {
+    __REVISION__: false
+  }
+};
---
+++ prettier/index.js
@@ -0,0 +1,15 @@
+// prettier config
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "preserve",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid"
+};
