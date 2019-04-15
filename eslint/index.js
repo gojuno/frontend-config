51885e01760d46b3e49d92e6cbcde9272e34e682
Anatoli Huseu <anatoli.huseu@gojuno.com>
2019-04-15 14:49:40 +0300

    add configs

---
+++ eslint/index.js
@@ -0,0 +1,26 @@
+// eslint config
+
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "rules": {
+    "prefer-const": "warn",
+    "react/prop-types": ["warn", {
+      "ignore": ["dispatch"]
+    }],
+    "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
+    "react/jsx-no-target-blank": "warn"
+  },
+  "globals": {
+    "__REVISION__" : false
+  }
+}
---
+++ prettier/index.js
@@ -0,0 +1,15 @@
+// prettier config
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'preserve',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
